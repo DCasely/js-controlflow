3e545db1058d4007de394da9fe8497df14bafd80
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-02-04 19:38:59 -0500

    Super Bonus added

--- script.js
+++ script.js
@@ -6,4 +6,6 @@ if (age < 0) {
   console.log('happy 21st birthday!!');
 } else if (age % 2 !== 0) {
   console.log('your age is odd!');
+} else if (age % Math.sqrt(age) === 0) {
+  console.log('Your age is a perfect square!');
 }
